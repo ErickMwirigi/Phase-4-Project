de0a20d83cf7a68ac245992ecb8c385d534592ce
MelvinMbae <melvinmuthomi95@gmail.com>
2023-11-28 00:19:46 +0300

    Cover component

--- client/src/components/Cover.js
+++ client/src/components/Cover.js
@@ -6,12 +6,13 @@ function Cover() {
             <div className="cover">
                 <div className="title">
                     <div className="heading">
-                        The Music Bar
+                        Our Market Place
                         <img className="image" src="./shopping-cart.png" alt="shopping cart" />
                     </div>
                     <div className="tagline">
-                        A Music community where you can review your best albums and follow
-                        your favorite artists
+                        With a commitment to quality and customer satisfaction, discover a world of possibilities tailored to your needs.
+                        Seamless Shopping, Limitless Options
+                        Innovate Your Shopping Experience
                     </div>
                 </div>
             </div>
