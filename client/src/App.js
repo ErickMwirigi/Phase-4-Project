9e601fc0d23a3017422bddf06994ec84f8cad89c
MelvinMbae <melvinmuthomi95@gmail.com>
2023-11-27 23:51:42 +0300

    node modules installed and components folder created

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import Erick from "./Erick";
+
+
+// import './App.css';
+
+
+const productURL = "http://localhost:3000/products";
+
+
+
 
 function App() {
   return (
