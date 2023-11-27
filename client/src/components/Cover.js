7e51ebac17203e67dc84a9d1103510396300e193
MelvinMbae <melvinmuthomi95@gmail.com>
2023-11-28 00:07:19 +0300

    commit

---
+++ client/src/components/Cover.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Cover() {
+    return (
+        <div className="homepage">
+            <div className="cover">
+                <div className="title">
+                    <div className="heading">
+                        The Music Bar
+                        <img className="image" src="./shopping-cart.png" alt="shopping cart" />
+                    </div>
+                    <div className="tagline">
+                        A Music community where you can review your best albums and follow
+                        your favorite artists
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Cover;
