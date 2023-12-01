03bdc73e8b157a7ef06bc19f09beacf86fc12e2d
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-12-02 01:38:04 +0300

    A/C profile update

--- client/src/components/AccountProfile.js
+++ client/src/components/AccountProfile.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import DynamicMenu from './DynamicMenu'
 import { ReactComponent as Cart } from '../Cart.svg'
+import { Route , Routes } from "react-router-dom";
 
-export default function AccountProfile() {
+export default function AccountProfile({ customerData={'name':'Michael Njogu', "email":'michaelfebzen.mb@gmail.com',"profilePhoto":"https://upload.wikimedia.org/wikipedia/commons/9/9e/Placeholder_Person.jpg"}}) {
+
+
+// <Routes>
+//     <Route index />
+//     <Route path="/inbox" element={<LogIn />}/>
+//     <Route path="/orders" element={<SignUp />}/>
+//     <Route path="/saved-items" element={ <Cover />}/>
+//     <Route path="/profile-settings" element={ <AccountProfile />}/>
+// </Routes>
+
   return (
     <div className="account-profile">
         <div className="profile-header">
-            <img className="profile-photo"/>
+            <img className="profile-photo" src={customerData.profilePhoto}/>
             <div className="profile-box">
-                <p> Name</p>
-                <p>Email Address</p>
+                <p> {customerData.name}</p>
+                <p>{customerData.email}</p>
                 <br/>
                 <div className="Checkout"><Cart /> Cart Counter </div>
             </div>
