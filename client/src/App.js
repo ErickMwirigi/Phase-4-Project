d19af05d96968892a41d1371c10d3e61657b2244
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-12-04 20:40:59 +0300

    Complete Nabvbar update

--- client/src/App.js
+++ client/src/App.js
@@ -69,7 +69,6 @@ function onSearch(searched){
 
   return (
     <div className='first-page'>
-    <NavBar onSerach={onSearch}/>
       <Routes>
         <Route path="/" element={ isMember ? <LogIn /> : <SignUp />}/>
         <Route path="/login" element={ <LogIn />}/>
