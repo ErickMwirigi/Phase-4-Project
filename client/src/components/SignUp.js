d2d274803986e2a5f1884e9d0d03c654045d25d6
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-12-08 20:50:03 +0300

    Signup Comp Update

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -3,9 +3,9 @@ import { useState } from 'react'
 
 export default function SignUp({ formdata }){ 
 
-    // const [confirmPass, setConfirmPass] = useState("")
     const [formData, setFormData] = useState({
-        name: "",
+        firstname: "",
+        lastname: "",
         email: "",
         address:"",
         password:"",
@@ -23,7 +23,7 @@ export default function SignUp({ formdata }){
         e.preventDefault()
         // if(!formData) return 
 
-        alert(`Welcome ${formData.name}`)
+        alert(`Welcome ${formData.firstname}`)
         console.log(formData)
         fetch("http://127.0.0.1:5555/customers",{
             method:"POST",
@@ -43,11 +43,20 @@ export default function SignUp({ formdata }){
             <div className='form-dialogue'>
                 <form onSubmit={handleSubmit}>
                     <h2>Welcome to Our App!</h2>
-                    <label htmlFor='name'> name :
+                    <label htmlFor='firstname'> Firstname :
                         <input
                         type="text"
-                        id="name"
-                        value={formData.name}
+                        id="firstname"
+                        value={formData.firstname}
+                        autoComplete="off"
+                        onChange={handleChange}
+                        />
+                    </label>
+                    <label htmlFor='lastname'> Lastname :
+                        <input
+                        type="text"
+                        id="lastname"
+                        value={formData.lastname}
                         autoComplete="off"
                         onChange={handleChange}
                         />
