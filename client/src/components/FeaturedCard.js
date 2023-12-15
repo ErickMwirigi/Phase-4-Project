82dbdcd8f81e03565357d186810c7ff0275edd66
IsaacKivuva <136487354+IsaacKivuva@users.noreply.github.com>
2023-12-15 21:02:01 +0300

    Revert "readme.md"

--- client/src/components/FeaturedCard.js
+++ client/src/components/FeaturedCard.js
@@ -1,20 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-function FeaturedCards({featured}){
-
-    
-
-    return(
-        <>
-        <div>
-            <Link to={`/products/featured/${featured.id}`}>
-                <img src={featured.imageUrl} alt="Product" className="featured-images"/>
-            </Link>
-            <h4>{featured.name}</h4>
-        </div>
-        </>
-    )
+function FeaturedCards({ featured }) {
+  return (
+    <>
+      <img src={featured.imageUrl} alt="Product" />
+      <h4>{featured.name}</h4>
+    </>
+  );
 }
 
 export default FeaturedCards;
